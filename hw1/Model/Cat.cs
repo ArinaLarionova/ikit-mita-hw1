9bc0212a1d4645a300747796a4ab1077c574133b
ArinaLarionova <linnira32@gmail.com>
2017-06-12 15:25:21 +0700

    first

---
+++ hw1/Model/Cat.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Model
+{
+    public class Cat
+    {
+        
+        private string _name;
+        private int _health;
+
+        public Cat(int age, int health)
+        {
+            Age = age;
+            _health = health;
+        }
+
+        public string Name
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(_name))
+                {
+                    _name = value;
+                }
+            }
+            get { return _name; }
+        }
+        
+        public int Age { get; private set; }
+
+        public string Feed()
+        {
+            _health = _health + 1;
+            return "I like it!";
+        }
+
+        public string Punish()
+        {
+            _health = _health - 1;
+            return "I don't like it!";
+        }
+
+        public string Color
+        {
+            get
+            {
+                if (_health > 5)
+                {
+                    return "Green";
+                }
+                if (_health == 5)
+                {
+                    return "Yellow";
+                }
+                return "Red";
+            }
+        }
+    }
+}
---
+++ hw1/View/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace View
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            int menu;
+            int submenu = 4;
+            Console.WriteLine("Buy a cat!");
+            Console.WriteLine("Click 1 for start!");
+            menu = Convert.ToInt16(Console.ReadLine());
+            while (true)
+            {
+                switch (menu)
+                {
+                    case 1:
+
+                        Console.WriteLine("Set age");
+                        Console.WriteLine("Can only be set once");
+                        age = Convert.ToInt32(Console.ReadLine());
+                        if (age <= 0)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Age can not be 0");
+                        }
+                        else
+                        {
+                            var cat = new Cat(age, 5);
+                            while (menu != 0)
+                            {
+                                Console.Clear();
+                                if (string.IsNullOrEmpty(cat.Name) == false)
+                                    Console.WriteLine(string.Format("Name of cat " + cat.Name));
+                                Console.WriteLine(string.Format("Сat's age is " + cat.Age + " months"));
+
+                                Console.WriteLine("................................................................");
+                                Console.WriteLine("1 - set name");
+                                Console.WriteLine("2 - color of cat");
+                                Console.WriteLine("3 - punish");
+                                Console.WriteLine("4 - feed");
+                                menu = Convert.ToInt32(Console.ReadLine());
+                                switch (menu)
+                                {
+                                    case 1:
+                                        Console.Clear();
+                                        Console.WriteLine("Set name");
+                                        Console.WriteLine("Can only be set once");
+                                        if (string.IsNullOrEmpty(cat.Name) == true)
+                                            cat.Name = Console.ReadLine();
+                                        else
+                                        {
+                                            while (submenu != 0)
+                                            {
+                                                Console.Clear();
+                                                Console.WriteLine("Name is already set");
+                                                Console.WriteLine("0 to exit");
+                                                submenu = Convert.ToInt32(Console.ReadLine());
+                                            }
+                                            submenu = 4;
+                                        }
+                                        break;
+                                    case 2:
+                                        while (submenu != 0)
+                                        {
+                                            Console.Clear();
+                                            Console.WriteLine(cat.Color);
+                                            Console.WriteLine("0 to exit");
+                                            submenu = Convert.ToInt32(Console.ReadLine());
+                                        }
+                                        submenu = 4;
+                                        break;
+
+                                    case 3:
+                                        while (submenu != 0)
+                                        {
+
+                                            Console.Clear();
+                                            Console.WriteLine(cat.Punish());
+                                            Console.WriteLine("0 to exit");
+                                            submenu = Convert.ToInt32(Console.ReadLine());
+                                            break;
+
+                                        }
+                                        submenu = 4;
+                                        break;
+                                    case 4:
+                                        while (submenu != 0)
+                                        {
+                                            Console.Clear();
+                                            Console.WriteLine(cat.Feed());
+                                            Console.WriteLine("0 to exit");
+                                            submenu = Convert.ToInt32(Console.ReadLine());
+                                        }
+                                        submenu = 4;
+                                        break;
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
